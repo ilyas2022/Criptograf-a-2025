e71cd6acca9eb87238c4d89da385d53fd42ce67a
Sergio Rodríguez Gallardo <srg692@inlumine.ual.es>
2025-11-27 19:17:50 +0100

    Prueba randrange

--- TestingTask05.py
+++ TestingTask05.py
@@ -1,8 +1,11 @@
 import numpy
 from functools import reduce
 import operator
+import random
 
 bits = numpy.random.randint(0,2,16)
 print(bits)
 
 print(reduce(operator.xor,[i for i,bit in enumerate(bits) if bit]))
+for i in range(99):
+    print(random.randrange(15))
